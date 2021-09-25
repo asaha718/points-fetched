b745fc72b5011ad51b9f50806d2704263b342197
Anug Saha <anugsaha@gmail.com>
2021-09-25 16:11:59 -0400

    create feedback for when transactions are created

--- app/controllers/transactions_controller.rb
+++ app/controllers/transactions_controller.rb
@@ -1,10 +1,15 @@
 class TransactionsController < ApplicationController
   def create
+    render json: Transaction.create(transactions_params)
   end
 
   def index
   end
 
   def spend
+  end
+
+  def transactions_params
+    params.require(:transactions).permit(:payer, :points, :timestamp)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
-  get 'transactions/create'
-  get 'transactions/index'
-  get 'transactions/spend'
+
+  resources :transactions, only: [:create, :index]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
