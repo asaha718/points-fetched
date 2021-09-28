5367b8ac0057919d7bf9cc2c81159d2802ae0876
Anug Saha <anugsaha@gmail.com>
2021-09-27 22:15:06 -0400

    create logic that adds used points to hash and breaks once all points are used

--- app/controllers/transactions_controller.rb
+++ app/controllers/transactions_controller.rb
@@ -4,13 +4,31 @@ class TransactionsController < ApplicationController
   end
 
   def index
-    render json: Transaction.select(:name, "SUM(points) AS points").group(:name)
+    render json: Transaction.select(:payer, "SUM(points) as points").group(:payer)
   end
 
   def spend
+    points_to_spend= params[:points].to_i
+    spent_points={}
+    if Transaction.pluck(:points).sum > points_to_spend
+      Transaction.order(timestamp: :asc).each do |transaction|
+        if transaction.points < points_to_spend
+          points_to_spend -= transaction.points
+          spent_points[transaction.payer] = (spent_points[transaction.payer] || 0) - transaction.points
+          transaction.destroy
+        else 
+          spent_points[transaction.payer] = (spent_points[transaction.payer] || 0) - points_to_spend
+          transaction.update(points: transaction.points- points_to_spend)
+          break
+        end
+      end
+      render json: spent_points
+    end
   end
+  
+  private
 
   def transactions_params
-    params.require(:transactions).permit(:payer, :points, :timestamp)
+    params.require(:transactions).permit(:payer, :points, :timestamp) #is there a way to mass incorporate say and point data?
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
 
-  resources :transactions, only: [:create, :index]
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :transactions, only: [:create, :index] do
+    collection do 
+      post :spend
+    end    
+  end
+
 end
